414e4f7a39796c96424b485abeb1c01858bd00dc
bilalkreeri <computersoftb@rediffmail.com>
2017-08-31 00:45:10 +0530

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -1,14 +1,14 @@
-var api = "http://bcn.pool.com:5555";
+var api = "http://xmrmine.pool.com:3333";
 
 var coinUnits = 1000000000000;
 
 var poolHost = "bcn.pool.com";
 
-var irc = "irc.freenode.net/#Bytecoin-pools";
+var irc = "irc.freenode.net/#monero-pools";
 
-var email = "support@bcn.pool.com";
+var email = "support@xmrmine.pool.com";
 
-var cryptonatorWidget = ["BCN-BTC", "BCN-USD", "BCN-EUR", "BCN-GBP"];
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
